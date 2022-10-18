c9d5ae42e01703c76d9dc4952fef4dfc01320693
zt <2570585492@qq.com>
2022-10-18 15:48:33 +0800

    feat:finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  title:'Mooc-UI',
   base:'/mooc-ui/',
   themeConfig: {
+    nav:[{
+      text: 'Home', link: '/'
+    }, {
+      text: 'Github', link: 'https://github.com/lucktt0/mooc-ui'
+    },{
+      text:'Npm',link: 'https://www.npmjs.com/package/mooc-ui11'
+    }],
     sidebar: [
       '/',
       '/componentDocs/card'
